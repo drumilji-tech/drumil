73abb5d491e9b1fabad7f3ece512aa61550344e8
drumilji-tech <74672375+drumilji-tech@users.noreply.github.com>
2025-05-09 16:04:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server          # Azure needs this
 
-app.layout = html.H1("Hi Team! From Dash on Azure! Built by Ishtiza")
+app.layout = html.H1("Hi Team! From Dash on Azure! Built by Drumil Joshi - M&D Analyst I")
 if __name__ == "__main__":
     app.run_server(debug=True)
